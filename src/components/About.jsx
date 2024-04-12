42ee9db5e769a426181c6e048f8e3161ffa90f1e
Gaurav8959 <163885071+Gaurav8959@users.noreply.github.com>
2024-04-12 15:57:56 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <>
     <div name="About" className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16'>
       <h1 className="text-3xl font-bold mb-5">About</h1>
-      <p className=''>Hello, I'm Akhil, a passionate Web developer with a keen eye for MERN Stack . With a background in IT, I strive to create impactful and visually stunning Software solutions that leave a lasting impression.</p>
+      <p className=''>Hello, I'm Gaurav, a passionate Web developer with a keen eye for MERN Stack . With a background in IT, I strive to create impactful and visually stunning Software solutions that leave a lasting impression.</p>
       <br />
 
       <h1 className="text-green-600 font-semibold text-xl">Education & Training</h1>
